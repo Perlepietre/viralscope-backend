c6bf6251e52dcdedbe5c34a29c6f9e9b85b8d781
Perlepietre <info@perlepietre.com>
2026-04-09 19:28:03 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const createSubscription = require('./api/create-subscription')
-const stripeWebhook = require('./api/stripe-webhook')
-const trackAccount = require('./api/track-account')
-
-module.exports = {
-  createSubscription,
-  stripeWebhook,
-  trackAccount
-}
